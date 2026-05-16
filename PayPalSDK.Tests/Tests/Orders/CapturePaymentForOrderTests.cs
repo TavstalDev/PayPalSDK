b79de731d661e540b7bf2aa5da4d6d9d1ad08446
tavstal <tavstal@gmail.com>
2026-05-16 16:05:09 +0200

    Add sample JSON files and implement CapturePaymentForOrderTests for various response scenarios

---
+++ PayPalSDK.Tests/Tests/Orders/CapturePaymentForOrderTests.cs
@@ -0,0 +1,154 @@
+using System.Net;
+using System.Net.Http.Json;
+using Tavstal.PayPalSDK.Models.Common;
+using Tavstal.PayPalSDK.Models.Orders;
+using Tavstal.PayPalSDK.Models.Orders.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Orders;
+
+public class CapturePaymentForOrderTests : TestBase
+{
+    public CapturePaymentForOrderTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Orders/Capture/sample1.json", "Resources/Orders/Capture/sample2.json", 
+        "Resources/Orders/Capture/sample3.json", "Resources/Orders/Capture/sample4.json", "Resources/Orders/Capture/sample5.json", "Resources/Orders/Capture/sample6.json", "Resources/Orders/Capture/sample7.json"]) { }
+    
+
+    [Fact(DisplayName = "Sample 1 - 201 - Capture Order - PayPal Wallet as Payment Source")]
+    public async Task CapturePayment_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCaptureRequest("5O190127TN364715T", null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var orderResponse = await response.Content.ReadFromJsonAsync<OrderBody>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Order ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Order Status: " + orderResponse.Status);
+        orderResponse.Links.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Links: ");
+        foreach (var link in orderResponse.Links!)
+            _testOutputHelper.WriteLine(link.ToString());
+    }
+    
+    [Fact(DisplayName = "Sample 2 - 200 - Capture Order - Idempotent Response with PayPal Wallet")]
+    public async Task CapturePayment_Sample2()
+    {
+        var resource = _resources[1];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCaptureRequest("5O190127TN364715T", null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var orderResponse = await response.Content.ReadFromJsonAsync<OrderBody>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Order ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Order Status: " + orderResponse.Status);
+        orderResponse.Links.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Links: ");
+        foreach (var link in orderResponse.Links!)
+            _testOutputHelper.WriteLine(link.ToString());
+    }
+    
+    [Fact(DisplayName = "Sample 3 - 201 - Capture Order - Minimal Representation with PayPal Wallet Vault ID")]
+    public async Task CapturePayment_Sample3()
+    {
+        var resource = _resources[2];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCaptureRequest("5O190127TN364715T", null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var orderResponse = await response.Content.ReadFromJsonAsync<OrderBody>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Order ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Order Status: " + orderResponse.Status);
+        orderResponse.Links.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Links: ");
+        foreach (var link in orderResponse.Links!)
+            _testOutputHelper.WriteLine(link.ToString());
+    }
+    
+    [Fact(DisplayName = "Sample 4 - 403 - Capture Order - 403 Forbidden Error - Invalid Card Vault ID")]
+    public async Task CapturePayment_Sample4()
+    {
+        var resource = _resources[3];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCaptureRequest("5O190127TN364715T", null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        var orderResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+    
+    [Fact(DisplayName = "Sample 5 - 404 - Capture Order - 404 Not Found Error - Resource Not Found")]
+    public async Task CapturePayment_Sample5()
+    {
+        var resource = _resources[4];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCaptureRequest("5O190127TN364715T", null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        var orderResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+    
+    [Fact(DisplayName = "Sample 6 - 422 - Capture Order - 422 - Payer Action Required")]
+    public async Task CapturePayment_Sample6()
+    {
+        var resource = _resources[5];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCaptureRequest("5O190127TN364715T", null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        var orderResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+    
+    [Fact(DisplayName = "Sample 7 - 422 - Capture Order - 422 Unprocessable Entity Error - Action Does Not Match Intent")]
+    public async Task CapturePayment_Sample7()
+    {
+        var resource = _resources[6];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCaptureRequest("5O190127TN364715T", null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        var orderResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+}
