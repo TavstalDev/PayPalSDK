56de18d0b79ce9bddf07140e95c421b1cc901a6d
tavstal <tavstal@gmail.com>
2026-05-16 16:02:29 +0200

    Add OrderTrackingRequestBody and OrderAddTrackingRequest classes

---
+++ PayPalSDK/Models/Orders/Bodies/OrderTrackingRequestBody.cs
@@ -0,0 +1,57 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Tavstal.PayPalSDK.Models.Common.Orders;
+
+namespace Tavstal.PayPalSDK.Models.Orders.Bodies;
+
+/// <summary>
+/// Represents the request body for adding or updating shipment tracking information to a PayPal order.
+/// </summary>
+[DataContract]
+public class OrderTrackingRequestBody
+{
+    /// <summary>
+    /// Gets or sets the shipment tracking number provided by the carrier.
+    /// </summary>
+    [JsonPropertyName("tracking_number")]
+    public required string TrackingNumber { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the shipping carrier identifier.
+    /// </summary>
+    /// <remarks>
+    /// This field is required and should be set to a value from the <see cref="Tavstal.PayPalSDK.Constants.Carriers"/> constants.
+    /// If the carrier is not available for your country, choose the global version of the carrier.
+    /// If your carrier name is not in the list, set carrier to OTHER and set carrier name in <see cref="CarrierNameOther"/>.
+    /// </remarks>
+    [JsonPropertyName("carrier")]
+    public required string Carrier { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the name of the carrier if it is not in the standard list of carriers.
+    /// </summary>
+    [JsonPropertyName("carrier_name_other")]
+    public string? CarrierNameOther { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the ID of the capture transaction for which tracking information is being provided.
+    /// </summary>
+    [JsonPropertyName("capture_id")]
+    public required string CaptureId { get; set; }
+    
+    /// <summary>
+    /// Gets or sets a value indicating whether PayPal should notify the payer about the shipment tracking update.
+    /// </summary>
+    /// <remarks>
+    /// When set to <c>true</c>, PayPal will send a notification to the payer with the shipment tracking details.
+    /// Defaults to <c>false</c>.
+    /// </remarks>
+    [JsonPropertyName("notify_payer")]
+    public bool NotifyPayer { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the list of items included in this shipment.
+    /// </summary>
+    [JsonPropertyName("items")]
+    public List<TrackingItem>? Items { get; set; }
+}
---
+++ PayPalSDK/Models/Orders/OrderAddTrackingRequest.cs
@@ -0,0 +1,38 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Tavstal.PayPalSDK.Http;
+using Tavstal.PayPalSDK.Models.Orders.Bodies;
+
+namespace Tavstal.PayPalSDK.Models.Orders;
+
+/// <summary>
+/// Represents an HTTP request to add or update shipment tracking information for a PayPal order.
+/// </summary>
+public class OrderAddTrackingRequest : HttpRequestBase<OrderBody>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OrderAddTrackingRequest"/> class.
+    /// </summary>
+    /// <param name="orderId">The unique identifier of the PayPal order to which tracking information will be added.</param>
+    /// <param name="requestBody">The <see cref="OrderTrackingRequestBody"/> containing the tracking details such as carrier, tracking number, and items.</param>
+    public OrderAddTrackingRequest(string orderId, OrderTrackingRequestBody requestBody) 
+        : base(HttpMethod.Post, $"/v2/checkout/orders/{orderId}/track")
+    {
+        Content = JsonContent.Create(requestBody, options: new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+    }
+
+    /// <summary>
+    /// Adds the PayPal Partner Attribution ID header to the request.
+    /// </summary>
+    /// <param name="paypalPartnerAttributionId">The PayPal Partner Attribution ID to be added.</param>
+    /// <returns>The current instance of <see cref="OrderAddTrackingRequest"/> for method chaining.</returns>
+    public OrderAddTrackingRequest PaypalPartnerAttributionId(string paypalPartnerAttributionId)
+    {
+        Headers.Add("PayPal-Partner-Attribution-Id", paypalPartnerAttributionId);
+        return this;
+    }
+}
