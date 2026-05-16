b1848ae07899fa17fb8d58f9b477f1071263fe17
tavstal <tavstal@gmail.com>
2026-05-16 16:02:40 +0200

    Add OrderUpdateTrackingRequest class

---
+++ PayPalSDK/Models/Orders/OrderUpdateTrackingRequest.cs
@@ -0,0 +1,40 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Tavstal.PayPalSDK.Http;
+using Tavstal.PayPalSDK.Models.Common;
+using Tavstal.PayPalSDK.Models.Orders.Bodies;
+
+namespace Tavstal.PayPalSDK.Models.Orders;
+
+/// <summary>
+/// Represents an HTTP PATCH request to update an existing shipment tracker for a PayPal order.
+/// </summary>
+public class OrderUpdateTrackingRequest : HttpRequestBase<OrderBody>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OrderUpdateTrackingRequest"/> class.
+    /// </summary>
+    /// <param name="orderId">The unique identifier of the PayPal order containing the tracker to be updated.</param>
+    /// <param name="trackerId">The unique identifier of the specific tracker/shipment to be updated within the order.</param>
+    /// <param name="operations">An array of <see cref="UpdateOperation"/> objects that define the changes to apply to the tracker using JSON Patch operations.</param>
+    public OrderUpdateTrackingRequest(string orderId, string trackerId, UpdateOperation[] operations) 
+        : base(HttpMethod.Patch, $"/v2/checkout/orders/{orderId}/trackers/{trackerId}")
+    {
+        Content = JsonContent.Create(operations, options: new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+    }
+
+    /// <summary>
+    /// Adds the PayPal Partner Attribution ID header to the request.
+    /// </summary>
+    /// <param name="paypalPartnerAttributionId">The PayPal Partner Attribution ID to be added.</param>
+    /// <returns>The current instance of <see cref="OrderUpdateTrackingRequest"/> for method chaining.</returns>
+    public OrderUpdateTrackingRequest PaypalPartnerAttributionId(string paypalPartnerAttributionId)
+    {
+        Headers.Add("PayPal-Partner-Attribution-Id", paypalPartnerAttributionId);
+        return this;
+    }
+}
