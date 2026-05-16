6537b55c85ab7e9bdc859a87881ec764058d7e60
tavstal <tavstal@gmail.com>
2026-05-16 16:03:47 +0200

    Add sample JSON files and update CreateOrderTests for various order creation scenarios

--- PayPalSDK.Tests/Tests/Orders/CreateOrderTests.cs
+++ PayPalSDK.Tests/Tests/Orders/CreateOrderTests.cs
@@ -1,67 +1,160 @@
 using System.Net;
 using System.Net.Http.Json;
-using System.Text;
 using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Common;
 using Tavstal.PayPalSDK.Models.Orders;
 using Tavstal.PayPalSDK.Models.Orders.Bodies;
 using Tavstal.PayPalSDK.Tests.Helpers;
-using Xunit;
 using Xunit.Abstractions;
 
 namespace Tavstal.PayPalSDK.Tests.Tests.Orders;
 
-public class CreateOrderTests
+public class CreateOrderTests : TestBase
 {
-    private readonly ITestOutputHelper _testOutputHelper;
-
-    public CreateOrderTests(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
-
-    private HttpResponseMessage Responder(HttpRequestMessage request)
-    {
-        var path = request.RequestUri?.AbsolutePath ?? string.Empty;
-        if (path.Contains("/v1/oauth2/token"))
-        {
-            var json = File.ReadAllText("Resources/Responses/authResponse.json");
-            return new HttpResponseMessage(HttpStatusCode.OK)
-            {
-                Content = new StringContent(json, Encoding.UTF8, "application/json")
-            };
-        }
-
-        if (path.Contains("/v2/checkout/orders"))
-        {
-            var json = File.ReadAllText("Resources/Responses/createOrderResponse.json");
-            return new HttpResponseMessage(HttpStatusCode.Created)
-            {
-                Content = new StringContent(json, Encoding.UTF8, "application/json")
-            };
-        }
-
-        return new HttpResponseMessage(HttpStatusCode.NotFound);
-    }
+    public CreateOrderTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Orders/Create/sample1.json", "Resources/Orders/Create/sample2.json", 
+        "Resources/Orders/Create/sample3.json", "Resources/Orders/Create/sample4.json", "Resources/Orders/Create/sample5.json", "Resources/Orders/Create/sample6.json"]) { }
     
 
-    [Fact(DisplayName = "Should create an order successfully")]
-    public async Task CreateOrder_Success()
+    [Fact(DisplayName = "Sample 1 - 200 - Create Order - PayPal Wallet as Payment Source, Resulting in PAYER_ACTION_REQUIRED Response")]
+    public async Task CreateOrder_Sample1()
     {
-        var client = FakeHttpHelpers.CreateClient(Responder);
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
         
-        string requestJson = await File.ReadAllTextAsync("Resources/Requests/createOrderRequest.json");
-        OrderCreateRequestBody? body = JsonConvert.DeserializeObject<OrderCreateRequestBody>(requestJson);
-        body.Should().NotBeNull("The provided json should be deserializable into an OrderCreateRequestBody"); 
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        OrderCreateRequestBody? body = JsonConvert.DeserializeObject<OrderCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull(); 
         var request = new OrderCreateRequest(body!);
 
         var response = await client.SendAsync(request);
-        response.StatusCode.Should().Be(HttpStatusCode.Created, "A successful order creation should return a 201 Created status code");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
         var orderResponse = await response.Content.ReadFromJsonAsync<OrderBody>();
-        orderResponse.Should().NotBeNull("Response content should be deserializable into an OrderBody");
+        orderResponse.Should().NotBeNull();
         
-        _testOutputHelper.WriteLine("Response Status Code: " + response.StatusCode);
-        _testOutputHelper.WriteLine("Order ID: " + orderResponse?.Id);
+        _testOutputHelper.WriteLine("Order ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Order Status: " + orderResponse.Status);
+        orderResponse.Links.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Links: ");
+        foreach (var link in orderResponse.Links!)
+            _testOutputHelper.WriteLine(link.ToString());
+    }
+
+    [Fact(DisplayName = "Sample 2 - 201 - Create and authorize an order for a PayPal wallet vaulted account holder passing a usage pattern indicator using stored credentials (Single Shot).")]
+    public async Task CreateOrder_Sample2()
+    {
+        var resource = _resources[1];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+        
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        OrderCreateRequestBody? body = JsonConvert.DeserializeObject<OrderCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull(); 
+        var request = new OrderCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var orderResponse = await response.Content.ReadFromJsonAsync<OrderBody>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Order ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Order Status: " + orderResponse.Status);
+        orderResponse.Links.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Links: ");
+        foreach (var link in orderResponse.Links!)
+            _testOutputHelper.WriteLine(link.ToString());
     }
     
-    // TODO: Implement more tests
+    [Fact(DisplayName = "Sample 3 - 201 - Create Order - Minimal Request and Response")]
+    public async Task CreateOrder_Sample3()
+    {
+        var resource = _resources[2];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+        
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        OrderCreateRequestBody? body = JsonConvert.DeserializeObject<OrderCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull(); 
+        var request = new OrderCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var orderResponse = await response.Content.ReadFromJsonAsync<OrderBody>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Order ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Order Status: " + orderResponse.Status);
+        orderResponse.Links.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Links: ");
+        foreach (var link in orderResponse.Links!)
+            _testOutputHelper.WriteLine(link.ToString());
+    }
+    
+    [Fact(DisplayName = "Sample 4 - 201 - Create Order - Buy Online Pickup In Store Shipping Type")]
+    public async Task CreateOrder_Sample4()
+    {
+        var resource = _resources[3];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+        
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        OrderCreateRequestBody? body = JsonConvert.DeserializeObject<OrderCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull(); 
+        var request = new OrderCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var orderResponse = await response.Content.ReadFromJsonAsync<OrderBody>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Order ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Order Status: " + orderResponse.Status);
+        orderResponse.Links.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Links: ");
+        foreach (var link in orderResponse.Links!)
+            _testOutputHelper.WriteLine(link.ToString());
+    }
+    
+    [Fact(DisplayName = "Sample 5 - 400 - Create Order - 400 Bad Request Error - Purchase Unit is Empty")]
+    public async Task CreateOrder_Sample5()
+    {
+        var resource = _resources[4];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+        
+        resource.JsonRequest.Should().Be("{}");
+        var request = new OrderCreateRequest(null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var orderResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+        orderResponse.Details.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Error Details: ");
+        foreach (var detail in orderResponse.Details!)
+            _testOutputHelper.WriteLine(detail.ToString());
+    }
+    
+    [Fact(DisplayName = "Sample 6 - 422 - Create Order - 422 Unprocessable Entity Error - Missing Pickup Address")]
+    public async Task CreateOrder_Sample6()
+    {
+        var resource = _resources[5];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+        
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        OrderCreateRequestBody? body = JsonConvert.DeserializeObject<OrderCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull(); 
+        var request = new OrderCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        var orderResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+        orderResponse.Details.Should().NotBeNull();
+        _testOutputHelper.WriteLine("Error Details: ");
+        foreach (var detail in orderResponse.Details!)
+            _testOutputHelper.WriteLine(detail.ToString());
+
+    }
 }
