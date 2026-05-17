22e08674643cb766c62f9b45ad4c6b2e31565310
tavstal <tavstal@gmail.com>
2026-05-17 16:50:37 +0200

    Add SubscriptionListTransactionResponse class for PayPal Billing API subscription transaction list

---
+++ PayPalSDK/Models/Common/Plans/SubscriptionListTransactionResponse.cs
@@ -0,0 +1,23 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Tavstal.PayPalSDK.Models.Common.Plans;
+
+/// <summary>
+/// Represents the response containing a list of transactions for a subscription from the PayPal Billing API.
+/// </summary>
+[DataContract]
+public class SubscriptionListTransactionResponse
+{
+    /// <summary>
+    /// Gets or sets the list of subscription transaction objects returned in this response.
+    /// </summary>
+    [JsonPropertyName("transactions")]
+    public List<Transaction>? Transactions { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the hypermedia links associated with this subscription transaction list response.
+    /// </summary>
+    [JsonPropertyName("links")]
+    public List<Link>? Links { get; set; }
+}
