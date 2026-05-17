5d5dbf4ef0d262eda7084fbae29f8d8baa233380
tavstal <tavstal@gmail.com>
2026-05-17 11:49:25 +0200

    Add properties to Webhook class for ID, URL, event types, and links

--- PayPalSDK/Models/Common/Webhooks/Webhook.cs
+++ PayPalSDK/Models/Common/Webhooks/Webhook.cs
@@ -12,16 +12,28 @@ namespace Tavstal.PayPalSDK.Models.Common.Webhooks;
 public class Webhook
 {
     /// <summary>
+    /// Gets or sets the unique identifier of the webhook.
+    /// </summary>
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+    
+    /// <summary>
     /// Gets or sets the name of the webhook event.
     /// </summary>
     [JsonPropertyName("name")]
-    public required string Name { get; set; }
+    public string? Name { get; set; }
 
     /// <summary>
     /// Gets or sets the description of the webhook event.
     /// </summary>
     [JsonPropertyName("description")]
     public string? Description { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the callback URL associated with the webhook.
+    /// </summary>
+    [JsonPropertyName("url")]
+    public string? Url { get; set; }
     
     /// <summary>
     /// Gets or sets the status of the webhook event.
@@ -34,4 +46,16 @@ public class Webhook
     /// </summary>
     [JsonPropertyName("resource_versions")]
     public List<string>? ResourceVersions { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of webhook event types subscribed to by this webhook.
+    /// </summary>
+    [JsonPropertyName("event_types")]
+    public List<WebhookEventType>? EventTypes { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the hypermedia links associated with the webhook.
+    /// </summary>
+    [JsonPropertyName("links")]
+    public List<Link>? Links { get; set; }
 }
