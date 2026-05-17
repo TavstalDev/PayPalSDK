4b4cb9c2c6d6ba053c85638496cb148a25374a4b
tavstal <tavstal@gmail.com>
2026-05-17 21:47:21 +0200

    Add InvoiceSendReminderTests and sample JSON for testing invoice reminder functionality

---
+++ PayPalSDK.Tests/Tests/Invoices/InvoiceSendReminderTests.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Invoices;
+using Tavstal.PayPalSDK.Models.Invoices.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Invoices;
+
+public class InvoiceSendReminderTests : TestBase
+{
+    public InvoiceSendReminderTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Invoices/SendReminder/sample1.json"]) { }
+    
+
+    [Fact(DisplayName = "Sample 1 - 204 - Remind Payer to Pay Invoice")]
+    public async Task InvoiceSendReminder_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<InvoiceSendReminderRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new InvoiceSendReminderRequest("INV2-Z56S-5LLA-Q52L-CPZ5", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        
+        _testOutputHelper.WriteLine("Invoice reminder sent successfully.");
+    }
+}
