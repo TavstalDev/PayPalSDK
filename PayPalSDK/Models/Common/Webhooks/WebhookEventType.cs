cea676f28a163b88a35a358b8097e5aa93b6be13
tavstal <tavstal@gmail.com>
2026-05-17 11:49:19 +0200

    Add WebhookEventType model to represent PayPal webhook event details

---
+++ PayPalSDK/Models/Common/Webhooks/WebhookEventType.cs
@@ -0,0 +1,29 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Tavstal.PayPalSDK.Models.Common.Webhooks;
+
+/// <summary>
+/// Represents a PayPal webhook event type.
+/// </summary>
+[DataContract]
+public class WebhookEventType
+{
+    /// <summary>
+    /// Gets or sets the webhook event name.
+    /// </summary>
+    /// <remarks>
+    /// This is the unique event type identifier used by PayPal to distinguish webhook events.
+    /// </remarks>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the webhook event description.
+    /// </summary>
+    /// <remarks>
+    /// This provides a human-readable explanation of what the event represents.
+    /// </remarks>
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+}
