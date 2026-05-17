8270bb3ca9bd82c9eb7357f1e42612ac913342b9
tavstal <tavstal@gmail.com>
2026-05-17 11:51:56 +0200

    Add WebhooksListTests and sample JSON for listing webhooks

---
+++ PayPalSDK.Tests/Tests/Webhooks/WebhooksListTests.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using System.Net.Http.Json;
+using Tavstal.PayPalSDK.Models.Webhooks;
+using Tavstal.PayPalSDK.Models.Webhooks.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Webhooks;
+
+public class WebhooksListTests : TestBase
+{
+    public WebhooksListTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Webhooks/List/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 200 - List All Webhooks")]
+    public async Task WebhooksList_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+        var request = new WebhookListRequest();
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var objectResponse = await response.Content.ReadFromJsonAsync<WebhookListResponseBody>();
+        objectResponse.Should().NotBeNull();
+        objectResponse!.Webhooks.Should().NotBeNull();
+        
+        foreach (var webhook in objectResponse!.Webhooks!)
+        {
+            _testOutputHelper.WriteLine("Id: " + webhook.Id);
+            _testOutputHelper.WriteLine("Url: " + webhook.Url);
+        }
+    }
+}
