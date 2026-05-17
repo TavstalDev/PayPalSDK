982d4bd3372b9e664523cad0e18070bf375ebaac
tavstal <tavstal@gmail.com>
2026-05-17 21:47:17 +0200

    Add InvoiceSendTests and sample JSON for testing invoice sending functionality

---
+++ PayPalSDK.Tests/Tests/Invoices/InvoiceSendTests.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Common;
+using Tavstal.PayPalSDK.Models.Invoices;
+using Tavstal.PayPalSDK.Models.Invoices.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Invoices;
+
+public class InvoiceSendTests : TestBase
+{
+    public InvoiceSendTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Invoices/Send/sample1.json"]) { }
+    
+
+    [Fact(DisplayName = "Sample 1 - 202 - Schedules the invoice when the issue date is in future.")]
+    public async Task InvoiceSend_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<InvoiceSendRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new InvoiceSendRequest("INV2-EHNV-LJ5S-A7DZ-V6NJ", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        var orderResponse = await response.Content.ReadFromJsonAsync<Link>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Url: " + orderResponse!.Href);
+    }
+}
