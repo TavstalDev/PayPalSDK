9e3537025cb4420ecb7ff686f5cc95fa6bf3d5ea
tavstal <tavstal@gmail.com>
2026-05-17 16:52:50 +0200

    Add SubscriptionPlanUpdateTests for updating subscription plans with 204 response

---
+++ PayPalSDK.Tests/Tests/Subscriptions/Plan/SubscriptionPlanUpdateTests.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Common;
+using Tavstal.PayPalSDK.Models.Subscriptions.Plan;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Subscriptions.Plan;
+
+public class SubscriptionPlanUpdateTests : TestBase
+{
+    public SubscriptionPlanUpdateTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Subscriptions/Plan/Update/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 204 - Patch a plan")]
+    public async Task SubscriptionPlanUpdate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<List<UpdateOperation>>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new SubscriptionPlanUpdateRequest("P-7GL4271244454362WXNWU5NQ", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        
+        _testOutputHelper.WriteLine("Plan updated successfully.");
+    }
+}
