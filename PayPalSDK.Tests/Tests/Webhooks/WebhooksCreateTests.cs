d27fe4d74fb6e0dc98ac6892d10e9ef079d9c367
tavstal <tavstal@gmail.com>
2026-05-17 11:50:42 +0200

    Add WebhooksCreateTests and sample JSON for webhook creation

---
+++ PayPalSDK.Tests/Tests/Webhooks/WebhooksCreateTests.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Webhooks;
+using Tavstal.PayPalSDK.Models.Webhooks.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Webhooks;
+
+public class WebhooksCreateTests : TestBase
+{
+    public WebhooksCreateTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Webhooks/Create/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 201 - Create Webhook")]
+    public async Task WebhooksCreate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<WebhookCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new WebhookCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var objectResponse = await response.Content.ReadFromJsonAsync<WebhookBody>();
+        objectResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("ID: " + objectResponse!.Id);
+        _testOutputHelper.WriteLine("Url: " + objectResponse.Url);
+    }
+}
