c9626daeda6d17b5c1a2163a71f704bd4e8058d9
tavstal <tavstal@gmail.com>
2026-05-17 16:52:41 +0200

    Add SubscriptionPlanPricingTests for updating multiple tiered pricing schemes with 204 response

---
+++ PayPalSDK.Tests/Tests/Subscriptions/Plan/SubscriptionPlanPricingTests.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Common.Plans;
+using Tavstal.PayPalSDK.Models.Subscriptions.Plan;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Subscriptions.Plan;
+
+public class SubscriptionPlanPricingTests : TestBase
+{
+    public SubscriptionPlanPricingTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Subscriptions/Plan/Pricing/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 204 - Add multiple tiered pricing schemes")]
+    public async Task SubscriptionPlanPricing_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<List<PlanPricingScheme>>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new SubscriptionPlanPricingUpdateRequest("P-7GL4271244454362WXNWU5NQ", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        
+        _testOutputHelper.WriteLine("Pricing schemes updated successfully.");
+    }
+}
