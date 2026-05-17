1b573ff0995fec6c4ee97359c28f59b5e71e048e
tavstal <tavstal@gmail.com>
2026-05-17 11:51:50 +0200

    Add WebhooksGetTests and sample JSON for webhook retrieval

---
+++ PayPalSDK.Tests/Tests/Webhooks/WebhooksGetTests.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using System.Net.Http.Json;
+using Tavstal.PayPalSDK.Models.Webhooks;
+using Tavstal.PayPalSDK.Models.Webhooks.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Webhooks;
+
+public class WebhooksGetTests : TestBase
+{
+    public WebhooksGetTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Webhooks/Get/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 200 - Show Webhook Details")]
+    public async Task WebhooksCreate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new WebhookGetRequest("0EH40505U7160970P");
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var objectResponse = await response.Content.ReadFromJsonAsync<WebhookBody>();
+        objectResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("ID: " + objectResponse!.Id);
+        _testOutputHelper.WriteLine("Url: " + objectResponse.Url);
+    }
+}
