3057a540db81524ec3b658e61ec9c70d27398ce3
tavstal <tavstal@gmail.com>
2026-05-17 11:49:53 +0200

    Add ProductCreateTests and sample JSON for product creation

---
+++ PayPalSDK.Tests/Tests/ProductCatalog/ProductCreateTests.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.ProductCatalog;
+using Tavstal.PayPalSDK.Models.ProductCatalog.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.ProductCatalog;
+
+public class ProductCreateTests : TestBase
+{
+    public ProductCreateTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Products/Create/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 201 - Create Product")]
+    public async Task ProductCreate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<ProductBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new ProductCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var objectResponse = await response.Content.ReadFromJsonAsync<ProductBody>();
+        objectResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("ID: " + objectResponse!.Id);
+        _testOutputHelper.WriteLine("Name: " + objectResponse.Name);
+    }
+}
