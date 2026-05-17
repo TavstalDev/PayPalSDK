730db5d8d8e84442fb0a101d9c3494e475b28000
tavstal <tavstal@gmail.com>
2026-05-17 16:50:31 +0200

    Add SubscriptionListResponse class for PayPal Billing API subscription list endpoint

---
+++ PayPalSDK/Models/Common/Plans/SubscriptionListResponse.cs
@@ -0,0 +1,24 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Tavstal.PayPalSDK.Models.Subscriptions.Bodies;
+
+namespace Tavstal.PayPalSDK.Models.Common.Plans;
+
+/// <summary>
+/// Represents the response returned by the subscription list endpoint of the PayPal Billing API.
+/// </summary>
+[DataContract]
+public class SubscriptionListResponse
+{
+    /// <summary>
+    /// Gets or sets the list of subscription objects returned in this response.
+    /// </summary>
+    [JsonPropertyName("subscriptions")]
+    public List<SubscriptionBody>? Subscriptions { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the hypermedia links associated with this subscription list response.
+    /// </summary>
+    [JsonPropertyName("links")]
+    public List<Link>? Links { get; set; }
+}
