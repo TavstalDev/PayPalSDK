1eee21bfd9c4e82fb69a892f0955b1edcd637544
tavstal <tavstal@gmail.com>
2026-05-17 11:52:19 +0200

    Add WebhooksVerifyTests and sample JSON for webhook signature verification

---
+++ PayPalSDK.Tests/Tests/Webhooks/WebhooksVerifyTests.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Webhooks;
+using Tavstal.PayPalSDK.Models.Webhooks.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Webhooks;
+
+public class WebhooksVerifyTests : TestBase
+{
+    public WebhooksVerifyTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Webhooks/Verify/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 200 - Verify Webhook Signature")]
+    public async Task WebhooksVerify_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<WebhookVerifyRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new WebhookVerifyRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var objectResponse = await response.Content.ReadFromJsonAsync<WebhookVerifyResponseBody>();
+        objectResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Status: " + objectResponse!.VerificationStatus);
+    }
+}
