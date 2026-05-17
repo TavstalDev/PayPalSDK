a665250acd19b15b1dd0d8600e263a35f53ec0c2
tavstal <tavstal@gmail.com>
2026-05-17 16:51:42 +0200

    Add SubscriptionCaptureTests for capturing payments with 202 response

---
+++ PayPalSDK.Tests/Tests/Subscriptions/SubscriptionCaptureTests.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Subscriptions;
+using Tavstal.PayPalSDK.Models.Subscriptions.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Subscriptions;
+
+public class SubscriptionCaptureTests : TestBase
+{
+    public SubscriptionCaptureTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Subscriptions/Capture/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 202 - Capture Payment for Subscription")]
+    public async Task SubscriptionCapture_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<SubscriptionCaptureRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new SubscriptionCaptureRequest("I-BW452GLLEP1G", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        
+        _testOutputHelper.WriteLine("Subscription capture request sent successfully.");
+    }
+}
