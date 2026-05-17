253e87619c357f686f52e9bc1a0ca021073dd349
tavstal <tavstal@gmail.com>
2026-05-17 16:51:31 +0200

    Add SubscriptionActivateTests for activating subscriptions with 204 response

---
+++ PayPalSDK.Tests/Tests/Subscriptions/SubscriptionActivateTests.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Subscriptions;
+using Tavstal.PayPalSDK.Models.Subscriptions.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Subscriptions;
+
+public class SubscriptionActivateTests : TestBase
+{
+    public SubscriptionActivateTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Subscriptions/Activate/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 204 - Activate subscription")]
+    public async Task SubscriptionActivate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<SubscriptionCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new SubscriptionActivateRequest("I-BW452GLLEP1G", "Test");
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        
+        _testOutputHelper.WriteLine("Subscription activated successfully");
+    }
+}
