06bcecec873ecd5ccb650e4f3ad9d82234988088
tavstal <tavstal@gmail.com>
2026-05-17 21:44:50 +0200

    Add DateRange class to represent a date-time range with start and end timestamps

---
+++ PayPalSDK/Models/Common/DateRange.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Tavstal.PayPalSDK.Models.Common;
+
+/// <summary>
+/// Represents a date-time range with a required start and end timestamp.
+/// </summary>
+[DataContract]
+public class DateRange
+{
+    /// <summary>
+    /// Gets or sets the start of the date range.
+    /// </summary>
+    [JsonPropertyName("start")]
+    [MinLength(20), MaxLength(64)]
+    [RegularExpression("^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])[T,t]([0-1][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)([.][0-9]+)?([Zz]|[+-][0-9]{2}:[0-9]{2})$")]
+    public required string Start { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the end of the date range.
+    /// </summary>
+    [JsonPropertyName("end")]
+    [MinLength(20), MaxLength(64)]
+    [RegularExpression("^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])[T,t]([0-1][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)([.][0-9]+)?([Zz]|[+-][0-9]{2}:[0-9]{2})$")]
+    public required string End { get; set; }
+}
