9f2aacd5f03c8287fc2c984c356319418042744b
tavstal <tavstal@gmail.com>
2026-05-17 16:51:48 +0200

    Add SubscriptionCreateTests for creating subscriptions with 201 response

---
+++ PayPalSDK.Tests/Tests/Subscriptions/SubscriptionCreateTests.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Subscriptions;
+using Tavstal.PayPalSDK.Models.Subscriptions.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Subscriptions;
+
+public class SubscriptionCreateTests : TestBase
+{
+    public SubscriptionCreateTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Subscriptions/Create/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 201 - Create Subscription for Plan")]
+    public async Task SubscriptionCreate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<SubscriptionCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new SubscriptionCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var objectResponse = await response.Content.ReadFromJsonAsync<SubscriptionBody>();
+        objectResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("ID: " + objectResponse!.Id);
+        _testOutputHelper.WriteLine("Status: " + objectResponse.Status);
+    }
+}
