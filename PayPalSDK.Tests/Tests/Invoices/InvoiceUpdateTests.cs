b15a296212228b072ea43a2fb83199235f09a339
tavstal <tavstal@gmail.com>
2026-05-17 21:47:32 +0200

    Add InvoiceUpdateTests and sample JSON for testing invoice updates

---
+++ PayPalSDK.Tests/Tests/Invoices/InvoiceUpdateTests.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Invoices;
+using Tavstal.PayPalSDK.Models.Invoices.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Invoices;
+
+public class InvoiceUpdateTests : TestBase
+{
+    public InvoiceUpdateTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Invoices/Update/sample1.json"]) { }
+    
+
+    [Fact(DisplayName = "Sample 1 - 200 - Update Invoice with Theme")]
+    public async Task InvoiceUpdate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<InvoiceUpdateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new InvoiceUpdateRequest("INV2-Z56S-5LLA-Q52L-CPZ5", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var orderResponse = await response.Content.ReadFromJsonAsync<InvoiceBody>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("ID: " + orderResponse!.Id);
+        _testOutputHelper.WriteLine("Status: " + orderResponse.Status);
+    }
+}
