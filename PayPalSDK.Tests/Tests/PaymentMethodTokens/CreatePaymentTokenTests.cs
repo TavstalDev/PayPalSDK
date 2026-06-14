4c143fc10f7c849f33e859c20a219278b46ebe21
tavstal <tavstal@gmail.com>
2026-06-15 00:06:15 +0200

    Add sample JSON files for Payment Method Tokens API responses and update test cases for retrieval and deletion scenarios

---
+++ PayPalSDK.Tests/Tests/PaymentMethodTokens/CreatePaymentTokenTests.cs
@@ -0,0 +1,156 @@
+using System.Net;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Common;
+using Tavstal.PayPalSDK.Models.PaymentMethodTokens;
+using Tavstal.PayPalSDK.Models.PaymentMethodTokens.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.PaymentMethodTokens;
+
+public class CreatePaymentTokenTests : TestBase
+{
+    public CreatePaymentTokenTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/PaymentMethodTokens/Create/sample1.json", "Resources/PaymentMethodTokens/Create/sample2.json", 
+        "Resources/PaymentMethodTokens/Create/sample3.json", "Resources/PaymentMethodTokens/Create/sample4.json", "Resources/PaymentMethodTokens/Create/sample5.json", 
+        "Resources/PaymentMethodTokens/Create/sample6.json", "Resources/PaymentMethodTokens/Create/sample7.json"]) { }
+    
+
+    [Fact(DisplayName = "Sample 1 - 201 - Create Payment Token - PayPal Wallet from Setup Token")]
+    public async Task CreatePaymentToken_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<PaymentTokenCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+
+        var request = new PaymentTokenCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var orderResponse = await request.GetResponseBodyAsync(response);
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("ID: " + orderResponse!.Id);
+    }
+    
+    [Fact(DisplayName = "Sample 2 - 200 - Create Payment Token - Card with Full Request - Idempotent")]
+    public async Task CreatePaymentToken_Sample2()
+    {
+        var resource = _resources[1];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<PaymentTokenCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+
+        var request = new PaymentTokenCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var orderResponse = await request.GetResponseBodyAsync(response);
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("ID: " + orderResponse!.Id);
+    }
+    
+    [Fact(DisplayName = "Sample 3 - 400 - Create Payment Token - 400 Invalid Request Error - Empty Request Body")]
+    public async Task CreatePaymentToken_Sample3()
+    {
+        var resource = _resources[2];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().Be("{}");
+        var request = new PaymentTokenCreateRequest(null!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var orderResponse = await response.Content.ReadJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+    
+    [Fact(DisplayName = "Sample 4 - 403 - Create Payment Token - 403 Forbidden Error - Merchant Not Onboarded to Vault")]
+    public async Task CreatePaymentToken_Sample4()
+    {
+        var resource = _resources[3];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<PaymentTokenCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+       
+        var request = new PaymentTokenCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        var orderResponse = await response.Content.ReadJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+    
+    [Fact(DisplayName = "Sample 5 - 404 - Create Payment Token - 404 Not Found Error - Setup Token Does Not Exist")]
+    public async Task CreatePaymentToken_Sample5()
+    {
+        var resource = _resources[4];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        var body = JsonConvert.DeserializeObject<PaymentTokenCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+       
+        var request = new PaymentTokenCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        var orderResponse = await response.Content.ReadJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+    
+    [Fact(DisplayName = "Sample 6 - 422 - Create Payment Token - 422 Unprocessable Entity Error - Inactive Billing Agreement")]
+    public async Task CreatePaymentToken_Sample6()
+    {
+        var resource = _resources[5];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        var body = JsonConvert.DeserializeObject<PaymentTokenCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+       
+        var request = new PaymentTokenCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.UnprocessableContent);
+        var orderResponse = await response.Content.ReadJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+    
+    [Fact(DisplayName = "Sample 7 - 500 - Create Payment Token - 500 Internal Server Error")]
+    public async Task CreatePaymentToken_Sample7()
+    {
+        var resource = _resources[6];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        var body = JsonConvert.DeserializeObject<PaymentTokenCreateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+       
+        var request = new PaymentTokenCreateRequest(body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        var orderResponse = await response.Content.ReadJsonAsync<ErrorResponse>();
+        orderResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Error Name: " + orderResponse!.Name);
+        _testOutputHelper.WriteLine("Error Message: " + orderResponse.Message);
+    }
+}
