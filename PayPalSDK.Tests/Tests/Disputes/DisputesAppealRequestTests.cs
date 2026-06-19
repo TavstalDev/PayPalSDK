8aff1828f06686074e0e1ef955cceda3a3e88570
tavstal <tavstal@gmail.com>
2026-06-19 18:45:50 +0200

    Add sample JSON files and test cases for dispute operations

---
+++ PayPalSDK.Tests/Tests/Disputes/DisputesAppealRequestTests.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Disputes;
+using Tavstal.PayPalSDK.Models.Disputes.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Disputes;
+
+public class DisputesAppealRequestTests : TestBase
+{
+    public DisputesAppealRequestTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Disputes/AppealRequest/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 200 - Appeal Dispute")]
+    public async Task AppealRequest_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<DisputeAppealRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new DisputeAppealRequest("PP-D-27803", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var objectResponse = await request.GetResponseBodyAsync(response);
+        objectResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Links: " + objectResponse!.Links!.Count);
+    }
+}
---
+++ PayPalSDK.Tests/Tests/Disputes/DisputesEscalateTests.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using Newtonsoft.Json;
+using Tavstal.PayPalSDK.Models.Disputes;
+using Tavstal.PayPalSDK.Models.Disputes.Bodies;
+using Tavstal.PayPalSDK.Tests.Helpers;
+using Xunit.Abstractions;
+
+namespace Tavstal.PayPalSDK.Tests.Tests.Disputes;
+
+public class DisputesEscalateTests : TestBase
+{
+    public DisputesEscalateTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, ["Resources/Disputes/Escalate/sample1.json"]) { }
+    
+    [Fact(DisplayName = "Sample 1 - 200 - Escalate Dispute to Claim")]
+    public async Task Escalate_Sample1()
+    {
+        var resource = _resources[0];
+        var client = FakeHttpHelpers.CreateClient(resource.Responder);
+
+        resource.JsonRequest.Should().NotBeNullOrEmpty();
+        var body = JsonConvert.DeserializeObject<DisputeEscalateRequestBody>(resource.JsonRequest!);
+        body.Should().NotBeNull();
+        
+        var request = new DisputeEscalateRequest("PP-000-000-651-454", body!);
+
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var objectResponse = await request.GetResponseBodyAsync(response);
+        objectResponse.Should().NotBeNull();
+        
+        _testOutputHelper.WriteLine("Links: " + objectResponse!.Links!.Count);
+    }
+}
