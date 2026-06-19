7c1b8861a24ca8c89ec78f3f5ab765c9ccefd1bb
tavstal <tavstal@gmail.com>
2026-06-19 18:44:32 +0200

    Add shipment information and claim acceptance request body models for disputes

---
+++ PayPalSDK/Models/Common/Disputes/ShipmentInfo.cs
@@ -0,0 +1,23 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Tavstal.PayPalSDK.Models.Common.Disputes;
+
+/// <summary>
+/// Represents shipment information related to a PayPal dispute, including the shipment label and tracking information.
+/// </summary>
+[DataContract]
+public class ShipmentInfo
+{
+    /// <summary>
+    /// Gets or sets the shipment label associated with the disputed item.
+    /// </summary>
+    [JsonPropertyName("shipment_label")]
+    public required NameObj ShipmentLabel { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the tracking information for the shipment, including carrier and tracking number.
+    /// </summary>
+    [JsonPropertyName("tracking_info")]
+    public required TrackingInfo TrackingInfo { get; set; }
+}
---
+++ PayPalSDK/Models/Common/NameObj.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Tavstal.PayPalSDK.Models.Common;
+
+/// <summary>
+/// Represents a name object used in PayPal SDK models.
+/// </summary>
+[DataContract]
+public class NameObj
+{
+    /// <summary>
+    /// Gets or sets the name value. This property can be null and has a maximum length of 2000 characters.
+    /// </summary>
+    [JsonPropertyName("name")]
+    [StringLength(2000)]
+    public string? Name { get; set; }
+}
