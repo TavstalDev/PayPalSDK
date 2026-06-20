7fa4ef70bb1ab6d9292c3e26a6fc858158d09f0a
tavstal <tavstal@gmail.com>
2026-06-20 17:54:16 +0200

    Rename OrderUpdateCallback to OrderReceiveUpdateCallback for clarity

--- PayPalSDK/Models/Orders/OrderReceiveUpdateCallback.cs
+++ PayPalSDK/Models/Orders/OrderReceiveUpdateCallback.cs
@@ -8,13 +8,13 @@ namespace Tavstal.PayPalSDK.Models.Orders;
 /// <summary>
 /// Represents an HTTP request to trigger or send an order update callback to the PayPal API.
 /// </summary>
-public class OrderUpdateCallback : HttpRequestBase<OrderBody>
+public class OrderReceiveUpdateCallback : HttpRequestBase<OrderBody>
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="OrderUpdateCallback"/> class.
+    /// Initializes a new instance of the <see cref="OrderReceiveUpdateCallback"/> class.
     /// </summary>
     /// <param name="requestBody">The <see cref="OrderUpdateCallbackRequestBody"/> containing the callback details and order update information to send to PayPal.</param>
-    public OrderUpdateCallback(OrderUpdateCallbackRequestBody requestBody) 
+    public OrderReceiveUpdateCallback(OrderUpdateCallbackRequestBody requestBody) 
         : base(HttpMethod.Post, "/v2/checkout/orders/order-update-callback")
     {
         Content = JsonContent.Create(requestBody, PayPalSDKJsonContext.Default.OrderUpdateCallbackRequestBody);
@@ -24,8 +24,8 @@ public class OrderUpdateCallback : HttpRequestBase<OrderBody>
     /// Adds the PayPal Partner Attribution ID header to the request.
     /// </summary>
     /// <param name="paypalPartnerAttributionId">The PayPal Partner Attribution ID to be added.</param>
-    /// <returns>The current instance of <see cref="OrderUpdateCallback"/> for method chaining.</returns>
-    public OrderUpdateCallback PaypalPartnerAttributionId(string paypalPartnerAttributionId)
+    /// <returns>The current instance of <see cref="OrderReceiveUpdateCallback"/> for method chaining.</returns>
+    public OrderReceiveUpdateCallback PaypalPartnerAttributionId(string paypalPartnerAttributionId)
     {
         Headers.Add("PayPal-Partner-Attribution-Id", paypalPartnerAttributionId);
         return this;
